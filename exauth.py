a29f1a319743bc322e5970f643b745bf5abb675c
Naoya Ikeda <naoya.ikeda@exchange.grayrecord.com>
2025-10-28 17:29:58 +0900

    Exchange 実装中

---
+++ exauth.py
@@ -0,0 +1,49 @@
+import httpx
+from dotenv import load_dotenv
+import os
+
+class ExchangeAuth:
+    client_id: str
+    client_secret: str
+    redirect_uri: str
+    auth_url: str
+    token_url: str
+
+    def __init__(self):
+        load_dotenv()
+        self.client_id = os.getenv("EXCHANGE_CLIENT_ID")
+        self.client_secret = os.getenv("EXCHANGE_CLIENT_SECRET")
+        self.redirect_uri = "https://login.microsoftonline.com/common/oauth2/nativeclient"
+        self.auth_url = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize"
+        self.token_url = "https://login.microsoftonline.com/common/oauth2/v2.0/token"
+    
+    def get_access_token(self, auth_code: str) -> dict:
+        data = {
+            "code": auth_code,
+            "scope":  "https://outlook.office365.com/IMAP.AccessAsUser.All offline_access",
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "redirect_uri": self.redirect_uri,
+            "grant_type": "authorization_code"
+        }
+        response = httpx.post(self.token_url, data=data)
+        response.raise_for_status()
+        return response.json()
+
+    def get_auth_url(self) -> str:
+        params = {
+            "client_id": self.client_id,
+            "response_type": "code",
+            "redirect_uri": self.redirect_uri,
+            "response_mode": "query",
+            "scope": "https://outlook.office365.com/IMAP.AccessAsUser.All offline_access",
+            "state": "12345"
+        }
+        request = httpx.Request("GET", self.auth_url, params=params)
+        return str(request.url)
+
+    def extract_auth_code(self, redirect_response: str) -> str:
+        from urllib.parse import urlparse, parse_qs
+        parsed_url = urlparse(redirect_response)
+        auth_code = parse_qs(parsed_url.query).get('code', [None])[0]
+        return auth_code
--- mail-crawler.py
+++ mail-crawler.py
@@ -4,6 +4,7 @@ from argparse import ArgumentParser
 import imaplib
 from dotenv import load_dotenv
 import gauth
+import exauth
 
 def add_account():
     config_path = "accounts.json"
@@ -15,6 +16,44 @@ def add_account():
     account_info['password'] = input("Enter password: ")
     account_info['imap_server'] = input("Enter IMAP server: ")
     account_info['imap_port'] = input("Enter IMAP port: ")
+
+    if os.path.exists(config_path):
+        with open(config_path, 'r') as f:
+            try:
+                accounts = json.load(f)
+            except json.JSONDecodeError:
+                accounts = []
+    else:
+        accounts = []
+
+    accounts.append(account_info)
+
+    with open(config_path, 'w') as f:
+        json.dump(accounts, f, indent=4)
+
+    print("Account added successfully.")
+
+def add_account_exchange():
+    exchange_auth = exauth.ExchangeAuth()
+
+    config_path = "accounts.json"
+    account_info = {}
+
+    account_info['auth_type'] = 'EXCHANGE_OAUTH2'
+    account_info['email'] = input("Enter email address: ")
+    account_info['username'] = account_info['email']
+    account_info['imap_server'] = "outlook.office365.com"
+    account_info['imap_port'] = 993
+
+    auth_url = exchange_auth.get_auth_url()
+    print("Please go to the following URL to authorize the application:")
+    print(auth_url)
+    auth_url = input("Enter the authorization url: ")
+
+    auth_code = exchange_auth.extract_auth_code(auth_url)
+
+    access_token_response = exchange_auth.get_access_token(auth_code)
+    account_info['password'] = access_token_response['access_token']
 
     if os.path.exists(config_path):
         with open(config_path, 'r') as f:
@@ -138,6 +177,8 @@ def execute_command(command):
         add_account()
     elif command == "add_gmail":
         add_account_gmail()
+    elif command == "add_exchange":
+        add_account_exchange()
     elif command == "list":
         list_accounts()
     elif command == "crawl":
@@ -149,7 +190,7 @@ def main():
     gmail_client_secret = os.getenv("GMAIL_CLIENT_SECRET")
 
     parser = ArgumentParser(description="Mail Crawler Configuration Loader")
-    parser.add_argument("command", choices=["clear", "add", 'add_gmail', "list", "crawl"], help="Command to execute")
+    parser.add_argument("command", choices=["clear", "add", "add_gmail", "add_exchange", "list", "crawl"], help="Command to execute")
 
     args = parser.parse_args()
     execute_command(args.command)

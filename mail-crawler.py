bc5adb4f7f0d46872fe204c0989880df46b1c538
Naoya Ikeda <naoya.ikeda@exchange.grayrecord.com>
2025-10-28 17:58:00 +0900

    大容量修正

--- mail-crawler.py
+++ mail-crawler.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 import gauth
 import exauth
 
+imaplib._MAXLINE = 10000000   # Increase the maximum line length for IMAP responses
+
 def add_account():
     config_path = "accounts.json"
     account_info = {}
